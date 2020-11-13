452a0edc22a807667c11c4a369098466328e3a7c
Alexander Fokin <alexanderfokindev@gmail.com>
2020-11-13 11:19:32 +0300

    Homework 2. Create a movie details activity

--- app/src/main/java/pt/amn/projectacademy/MainActivity.kt
+++ app/src/main/java/pt/amn/projectacademy/MainActivity.kt
@@ -1,11 +1,26 @@
 package pt.amn.projectacademy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pt.amn.projectacademy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // The variable needed for a view binding mechanism
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Initialize the binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btShowMovieDetails.setOnClickListener {
+            val intent = Intent(this, MovieDetailsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
